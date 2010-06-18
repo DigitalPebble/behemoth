83e3271f29b72e1078e7a7f181e8a0fd263f68d0
Julien Nioche <julien@digitalpebble.com>
2010-06-18 15:21:34 +0100

    Added Apache license in header where missing

--- src/main/java/com/digitalpebble/behemoth/io/nutch/NutchSegmentConverterJob.java
+++ src/main/java/com/digitalpebble/behemoth/io/nutch/NutchSegmentConverterJob.java
@@ -1,5 +1,3 @@
-package com.digitalpebble.behemoth.io.nutch;
-
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,9 @@ package com.digitalpebble.behemoth.io.nutch;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.digitalpebble.behemoth.io.nutch;
+
 
 import java.io.IOException;
 
