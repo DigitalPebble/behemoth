5ba4608a33bd044bca702a5b72392c8605633801
Mark Butler <markdigitalpebble@gmail.com>
2011-06-01 13:42:29 +0100

    Updated script for running some sample Behemoth tasks. This is very useful for testing purposes

--- script.sh
+++ script.sh
@@ -1,31 +1,50 @@
 # example of commands to run from Hadoop
 
-export behe_home=/data/behemoth-pebble
+export behe_home=${PWD}
+echo "Behemoth home is ${behe_home}"
 
-ant -f $behe_home/build.xml 
+if [ -n "${HADOOP_HOME+x}" ]; then
+echo "HADOOP_HOME is set to ${HADOOP_HOME}"
+else
+echo "HADOOP_HOME is not set - please configure to run script"
+exit
+fi
 
-hadoop jar $behe_home/build/behemoth-0.1-snapshot.job com.digitalpebble.behemoth.util.CorpusGenerator $behe_home/src/test/data/docs/ textcorpus
+export HADOOP=bin/hadoop
+export TESTDIR=test
+
+cd $HADOOP_HOME
+${HADOOP} fs -rmr ${TESTDIR}
+
+# load corpus into HDFS
+${HADOOP} jar ${behe_home}/modules/core/build/behemoth-core.job com.digitalpebble.behemoth.util.CorpusGenerator -i ${behe_home}/modules/gate/src/test/data/docs/ -o ${TESTDIR}/textcorpus
+
 # have a quick look at the content
-hadoop fs -libjars $behe_home/build/behemoth-0.1-snapshot.job -text textcorpus
-hadoop fs -copyFromLocal $behe_home/src/test/data/ANNIE.zip ANNIE.zip
-hadoop jar $behe_home/build/behemoth-0.1-snapshot.job com.digitalpebble.behemoth.gate.GATEDriver -conf $behe_home/conf/behemoth-site.xml textcorpus textcorpusANNIE ANNIE.zip
+${HADOOP} fs -libjars ${behe_home}/modules/core/build/behemoth-core.job -text ${TESTDIR}/textcorpus
+
+# need to call tika before GATE
+${HADOOP} jar ${behe_home}/modules/tika/build/behemoth-tika.job com.digitalpebble.behemoth.tika.TikaDriver -i ${TESTDIR}/textcorpus -o ${TESTDIR}/textCorpusProcessedWithTika
+
+# process dataset with GATE
+${HADOOP} fs -copyFromLocal ${behe_home}/modules/gate/src/test/data/ANNIE.zip ${TESTDIR}/ANNIE.zip
+${HADOOP} jar ${behe_home}/modules/gate/build/behemoth-gate.job com.digitalpebble.behemoth.gate.GATEDriver -conf ${behe_home}/modules/core/conf/behemoth-site.xml -i ${TESTDIR}/textCorpusProcessedWithTika -o ${TESTDIR}/textcorpusANNIE -g ${TESTDIR}/ANNIE.zip
+
 # have a look at the seqfile after processing
-hadoop fs -libjars $behe_home/build/behemoth-0.1-snapshot.job -text textcorpusANNIE/part-*
+${HADOOP} fs -libjars $behe_home/modules/core/build/behemoth-core.job -text ${TESTDIR}/textcorpusANNIE/part-*
 
 # processing a web archive
-hadoop fs -copyFromLocal $behe_home/src/test/data/ClueWeb09_English_Sample.warc ClueWeb09.warc
-hadoop jar $behe_home/build/behemoth-0.1-snapshot.job com.digitalpebble.behemoth.io.warc.WARCConverterJob -conf $behe_home/conf/behemoth-site.xml ClueWeb09.warc ClueWeb09
-hadoop jar $behe_home/build/behemoth-0.1-snapshot.job com.digitalpebble.behemoth.gate.GATEDriver -conf $behe_home/conf/behemoth-site.xml ClueWeb09 ClueWeb09Annie ANNIE.zip
+${HADOOP} fs -copyFromLocal $behe_home/modules/io/src/test/data/ClueWeb09_English_Sample.warc ${TESTDIR}/ClueWeb09.warc
+${HADOOP} jar $behe_home/modules/io/build/behemoth-io.job com.digitalpebble.behemoth.io.warc.WARCConverterJob -conf ${behe_home}/modules/core/conf/behemoth-site.xml -i ${TESTDIR}/ClueWeb09.warc -o ${TESTDIR}/ClueWeb09
+${HADOOP} jar $behe_home/modules/gate/build/behemoth-gate.job com.digitalpebble.behemoth.gate.GATEDriver -conf ${behe_home}/modules/core/conf/behemoth-site.xml -i ${TESTDIR}/ClueWeb09 -o ${TESTDIR}/ClueWeb09Annie -g ${TESTDIR}/ANNIE.zip
 
 # corpus reader (useful for older version of Hadoop e.g. 0.18.x)
-hadoop jar $behe_home/build/behemoth-0.1-snapshot.job  com.digitalpebble.behemoth.util.CorpusReader -conf $behe_home/conf/behemoth-site.xml ClueWeb09Annie
+${HADOOP} jar ${behe_home}/modules/core/build/behemoth-core.job com.digitalpebble.behemoth.util.CorpusReader -i ${TESTDIR}/ClueWeb09Annie
 
-# use of sandbox SOLR
-export behe_solr=/data/behemoth-pebble/sandbox/solr-indexer 
+# use of SOLR
+# commented out because SOLR server not running at 69.89.5.5
+#${HADOOP} jar ${behe_home}/modules/solr/build/behemoth-solr.job com.digitalpebble.behemoth.solr.SOLRIndexerJob -i ${TESTDIR}/ClueWeb09Annie -l http://69.89.5.5:8080/solr
 
-ant -f $behe_home/build.xml  jar
-ant -f $behe_solr/build.xml
-
-hadoop jar $behe_solr/build/behemoth-SOLR-0.1-snapshot.job com.digitalpebble.solr.SOLRIndexerJob ClueWeb09Annie http://69.89.5.5:8080/solr
-
+# process dataset with UIMA
+${HADOOP} fs -copyFromLocal ${behe_home}/modules/uima/src/test/data/WhitespaceTokenizer.pear ${TESTDIR}/WhitespaceTokenizer.pear
+${HADOOP} jar ${behe_home}/modules/uima/build/behemoth-uima.job com.digitalpebble.behemoth.uima.UIMADriver -conf ${behe_home}/modules/core/conf/behemoth-site.xml -i ${TESTDIR}/textCorpusProcessedWithTika -o ${TESTDIR}/textcorpusUIMA -p ${TESTDIR}/WhitespaceTokenizer.pear
 
