a74479f10a45627c9e02360b032389d3cb42e440
Mark Butler <markdigitalpebble@gmail.com>
2011-06-01 11:33:52 +0100

    Adding javadocs to CLI command line processor

--- modules/core/src/main/java/com/digitalpebble/behemoth/CliProcessor.java
+++ modules/core/src/main/java/com/digitalpebble/behemoth/CliProcessor.java
@@ -12,21 +12,37 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.lang.WordUtils;
 
+/**
+ * A basic standardised command line processor, based on Apache Commons CLI.
+ */
 public class CliProcessor {
 
 	Options options = new Options();
 
 	String name;
-	
+
 	String description;
 
 	CommandLine cli = null;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param name The command name.
+	 * @param description The command description.
+	 */
 	public CliProcessor(String name, String description) {
 		this.name = name;
 		this.description = description;
 	}
 
+	/**
+	 * Parse the command line arguments
+	 * 
+	 * @param args The command line arguments
+	 * @throws ParseException Thrown if the command line cannot be parsed e.g.
+	 *             is missing a required option or argument value.
+	 */
 	public void parse(String[] args) throws ParseException {
 		CommandLineParser parser = new GnuParser();
 		try {
@@ -37,8 +53,11 @@ public class CliProcessor {
 		}
 	}
 
+	/**
+	 * Print the usage information on this command to System.out.
+	 */
 	public void showUsage() {
-		System.out.println( WordUtils.wrap(name + ": " + description, 80));
+		System.out.println(WordUtils.wrap(name + ": " + description, 80));
 		StringBuilder use = new StringBuilder();
 		use.append(name);
 		List<String> requiredOptions = new ArrayList<String>(
@@ -61,31 +80,77 @@ public class CliProcessor {
 		}
 		new HelpFormatter().printHelp(use.toString(), options);
 	}
-	
+
+	/**
+	 * Get an argument value of an option.
+	 * 
+	 * @param optname The short name of the option.
+	 * @return The argument value.
+	 */
 	public String getOptionValue(String optname) {
 		return cli.getOptionValue(optname);
 	}
-	
+
+	/**
+	 * Has this option been set at the command line?
+	 * 
+	 * @param optname The short name of the option.
+	 * @return Has the option been set?
+	 */
 	public boolean hasOption(String optname) {
 		return cli.hasOption(optname);
 	}
-	
+
+	/**
+	 * Get the long name of the option.
+	 * 
+	 * @param optname The long name of the option.
+	 * @return The long name.
+	 */
 	public String getLongOpt(String optname) {
 		return options.getOption(optname).getLongOpt();
 	}
 
+	/**
+	 * Add a required option.
+	 * 
+	 * @param shortName The short name of the option.
+	 * @param longName The long name of the option.
+	 * @param description The option description.
+	 * @param hasArgument Does the option have an argument?
+	 * @return The option short name.
+	 */
 	public String addRequiredOption(String shortName, String longName,
 			String description, boolean hasArgument) {
 		return addOption(shortName, longName, description, hasArgument, true,
 				null);
 	}
 
+	/**
+	 * Add an option.
+	 * 
+	 * @param shortName The short name of the option.
+	 * @param longName The long name of the option.
+	 * @param description The option description.
+	 * @param hasArgument Does the option have an argument?
+	 * @return The option short name.
+	 */
 	public String addOption(String shortName, String longName,
 			String description, boolean hasArgument) {
 		return addOption(shortName, longName, description, hasArgument, false,
 				null);
 	}
 
+	/**
+	 * Add a required option.
+	 * 
+	 * @param shortName The short name of the option.
+	 * @param longName The long name of the option.
+	 * @param description The option description.
+	 * @param hasArgument Does the option have an argument?
+	 * @param argName The argument name.
+	 * @return The option short name.
+	 */
 	public String addRequiredOption(String shortName, String longName,
 			String description, boolean hasArgument, String argName) {
 		return addOption(shortName, longName, description, hasArgument, true,
@@ -107,16 +172,26 @@ public class CliProcessor {
 		return shortName;
 	}
 
+	/**
+	 * Get an argument value as an integer.
+	 * 
+	 * @param optname The option short name.
+	 * @param defaultvalue The default integer value.
+	 * @return The argument value as an integer.
+	 * @throws ParseArgumentException Thrown if the argument was not parseable
+	 *             as an integer.
+	 */
 	public int getIntArgument(String optname, int defaultvalue)
 			throws ParseArgumentException {
 		int result = defaultvalue;
 		String longoptname = options.getOption(optname).getLongOpt();
-		String value = cli.getOptionValue(optname); 
+		String value = cli.getOptionValue(optname);
 		if (value != null) {
 			try {
 				result = Integer.parseInt(value);
 			} catch (NumberFormatException e) {
-				throw new ParseArgumentException(longoptname, value, e.getMessage());
+				throw new ParseArgumentException(longoptname, value,
+						e.getMessage());
 			}
 		}
 		return result;
--- modules/core/src/main/java/com/digitalpebble/behemoth/InputOutputCliProcessor.java
+++ modules/core/src/main/java/com/digitalpebble/behemoth/InputOutputCliProcessor.java
@@ -1,23 +1,42 @@
 package com.digitalpebble.behemoth;
 
+/**
+ * A command line processor that includes input and output options intended to
+ * be used for paths on HDFS.
+ */
 public class InputOutputCliProcessor extends CliProcessor {
-	
+
 	String inputOpt;
-	
+
 	String outputOpt;
-	
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param name The command name.
+	 * @param description The command description.
+	 */
 	public InputOutputCliProcessor(String name, String description) {
 		super(name, description);
-		inputOpt = addRequiredOption("i", "input",
-				"Input path on HDFS", true);
+		inputOpt = addRequiredOption("i", "input", "Input path on HDFS", true);
 		outputOpt = addRequiredOption("o", "output",
 				"Output directory on HDFS", true);
 	}
-	
+
+	/**
+	 * Get the input path.
+	 * 
+	 * @return The input path.
+	 */
 	public String getInputValue() {
 		return cli.getOptionValue(inputOpt);
 	}
-	
+
+	/**
+	 * Get the output path.
+	 * 
+	 * @return The output path.
+	 */
 	public String getOutputValue() {
 		return cli.getOptionValue(outputOpt);
 	}
