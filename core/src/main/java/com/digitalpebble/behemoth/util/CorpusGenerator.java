a6478e0408045a0ee8ed8a2e6c870b1174adb611
Julien Nioche <julien@digitalpebble.com>
2012-08-06 10:41:23 +0100

    Issue #38 Warn when input is not available for CorpusGenerator

--- core/src/main/java/com/digitalpebble/behemoth/util/CorpusGenerator.java
+++ core/src/main/java/com/digitalpebble/behemoth/util/CorpusGenerator.java
@@ -183,6 +183,11 @@ public class CorpusGenerator extends Configured implements Tool {
         setInput(inputDir);
         setOutput(output);
 
+        if (inputDir.getFileSystem(getConf()).exists(inputDir)==false){
+            System.out.println("Input does not exist : "+inputDir);
+            return -1;
+        }
+        
         long count = generate(recurse);
         System.out.println(count + " docs converted");
         return 0;
