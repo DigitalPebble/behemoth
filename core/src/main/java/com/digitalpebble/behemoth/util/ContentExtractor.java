5795a690b397c00960fe34e6b189782e8b0af3f1
Julien Nioche <julien@digitalpebble.com>
2012-08-10 15:23:59 +0100

    Content extractor puts files in archives

--- core/src/main/java/com/digitalpebble/behemoth/util/ContentExtractor.java
+++ core/src/main/java/com/digitalpebble/behemoth/util/ContentExtractor.java
@@ -27,6 +27,10 @@ import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileStatus;
@@ -52,6 +56,8 @@ public class ContentExtractor extends Configured implements Tool {
     private static final Logger LOG = LoggerFactory
             .getLogger(ContentExtractor.class);
 
+    public static final String numEntriesPerArchiveParamName = "numEntriesPerArchive";
+
     public enum FileNamingMode {
         URL, UUID, NUM;
 
@@ -68,6 +74,12 @@ public class ContentExtractor extends Configured implements Tool {
 
     // dump the text otherwise
     private boolean dumpBinary = false;
+
+    private ArchiveOutputStream currentArchive = null;
+
+    private int partNum = 0;
+    private int numEntriesInCurrentArchive = 0;
+    private int maxNumEntriesInArchive = 10000;
 
     public ContentExtractor() {
     }
@@ -114,15 +126,16 @@ public class ContentExtractor extends Configured implements Tool {
                 mode = FileNamingMode.toMode(naming);
             }
 
-            generateDocs(input, output);
+            return generateDocs(input, output);
 
         } catch (ParseException e) {
             formatter.printHelp("ContentExtractor", options);
+            return -1;
         }
-        return 0;
     }
 
-    private void generateDocs(String inputf, String outputf) throws IOException {
+    private int generateDocs(String inputf, String outputf) throws IOException,
+            ArchiveException {
 
         Path input = new Path(inputf);
         Path dirPath = new Path(outputf);
@@ -133,7 +146,7 @@ public class ContentExtractor extends Configured implements Tool {
             fsout.mkdirs(dirPath);
         else {
             System.err.println("Output " + outputf + " already exists");
-            return;
+            return -1;
         }
 
         // index file
@@ -142,7 +155,12 @@ public class ContentExtractor extends Configured implements Tool {
             fsout.createNewFile(indexPath);
         }
 
+        maxNumEntriesInArchive = getConf().getInt(
+                numEntriesPerArchiveParamName, 10000);
+
         FSDataOutputStream index = fsout.create(indexPath);
+
+        createArchive(dirPath);
 
         FileSystem fs = input.getFileSystem(getConf());
         FileStatus[] statuses = fs.listStatus(input);
@@ -157,14 +175,44 @@ public class ContentExtractor extends Configured implements Tool {
 
         if (index != null)
             index.close();
+
+        if (currentArchive != null) {
+            currentArchive.finish();
+            currentArchive.close();
+        }
+
+        return 0;
+    }
+
+    private void createArchive(Path dirPath) throws IOException,
+            ArchiveException {
+        FileSystem fsout = FileSystem.get(dirPath.toUri(), getConf());
+        String archiveType = "zip";
+        FSDataOutputStream currentArchiveOS = fsout.create(new Path(dirPath,
+                "part_" + String.format("%06d", partNum) + "." + archiveType));
+        currentArchive = new ArchiveStreamFactory().createArchiveOutputStream(
+                archiveType, currentArchiveOS);
+        partNum++;
+        numEntriesInCurrentArchive = 0;
+    }
+
+    private void addToArchive(String fileName, byte[] content, Path dirPath)
+            throws IOException, ArchiveException {
+        numEntriesInCurrentArchive++;
+        currentArchive.putArchiveEntry(new ZipArchiveEntry(fileName));
+        currentArchive.write(content);
+        currentArchive.closeArchiveEntry();
+        if (numEntriesInCurrentArchive == maxNumEntriesInArchive) {
+            currentArchive.finish();
+            currentArchive.close();
+            createArchive(dirPath);
+        }
     }
 
     private void generateDocs(Path input, Path dir, int[] count,
-            FSDataOutputStream index) throws IOException {
+            FSDataOutputStream index) throws IOException, ArchiveException {
 
         DocumentFilter docFilter = DocumentFilter.getFilters(getConf());
-
-        FileSystem fsout = FileSystem.get(dir.toUri(), getConf());
 
         Reader[] cacheReaders = SequenceFileOutputFormat.getReaders(getConf(),
                 input);
@@ -172,7 +220,6 @@ public class ContentExtractor extends Configured implements Tool {
             // read the key + values in that file
             Text key = new Text();
             BehemothDocument inputDoc = new BehemothDocument();
-            FSDataOutputStream out = null;
             while (current.next(key, inputDoc)) {
                 count[0]++;
                 // filter the doc?
@@ -182,59 +229,34 @@ public class ContentExtractor extends Configured implements Tool {
                     continue;
                 else if (!dumpBinary && inputDoc.getText() == null)
                     continue;
-                try {
-                    String fileName = Integer.toString(count[0]);
-                    String urldoc = inputDoc.getUrl();
-                    if (mode.equals(FileNamingMode.URL) && urldoc != null
-                            && urldoc.length() > 0) {
-                        fileName = URLEncoder.encode(urldoc, "UTF-8");
-                    } else if (mode.equals(FileNamingMode.UUID)
-                            && urldoc != null && urldoc.length() > 0) {
-                        fileName = UUID.nameUUIDFromBytes(urldoc.getBytes())
-                                .toString();
-                    } else {
-                        fileName = String.format("%09d", count[0]);
-                    }
 
-                    if (!dumpBinary)
-                        fileName += ".txt";
-
-                    // TODO add suffix based on mimetype when dumping binary
-                    // content
-
-                    Path outFilePath = new Path(dir, fileName);
-                    if (fsout.exists(outFilePath) == false) {
-                        fsout.createNewFile(outFilePath);
-                    } else {
-                        // already there! prefix with global counter
-                        fileName = Integer.toString(count[0]) + "_" + fileName;
-                        outFilePath = new Path(dir, fileName);
-                        if (fsout.exists(outFilePath) == false) {
-                            fsout.createNewFile(outFilePath);
-                        }
-                    }
-
-                    out = fsout.create(outFilePath);
-
-                    byte[] contentBytes;
-                    if (dumpBinary)
-                        contentBytes = inputDoc.getContent();
-                    else
-                        contentBytes = inputDoc.getText().getBytes("UTF-8");
-
-                    out.write(contentBytes, 0, contentBytes.length);
-
-                    // add the mapping URL->filename in the index
-                    index.writeBytes(urldoc + "\t" + fileName + "\n");
-
-                } catch (Exception e) {
-                    LOG.error(
-                            "Exception on doc [" + count[0] + "] "
-                                    + key.toString(), e);
-                } finally {
-                    if (out != null)
-                        out.close();
+                String fileName = Integer.toString(count[0]);
+                String urldoc = inputDoc.getUrl();
+                if (mode.equals(FileNamingMode.URL) && urldoc != null
+                        && urldoc.length() > 0) {
+                    fileName = URLEncoder.encode(urldoc, "UTF-8");
+                } else if (mode.equals(FileNamingMode.UUID) && urldoc != null
+                        && urldoc.length() > 0) {
+                    fileName = UUID.nameUUIDFromBytes(urldoc.getBytes())
+                            .toString();
+                } else {
+                    fileName = String.format("%09d", count[0]);
                 }
+
+                if (!dumpBinary)
+                    fileName += ".txt";
+
+                byte[] contentBytes;
+                if (dumpBinary)
+                    contentBytes = inputDoc.getContent();
+                else
+                    contentBytes = inputDoc.getText().getBytes("UTF-8");
+                // out.write(contentBytes, 0, contentBytes.length);
+                addToArchive(fileName, contentBytes, dir);
+
+                // add the mapping URL->filename in the index -> archive num
+                index.writeBytes(urldoc + "\t" + fileName + "\t"
+                        + String.format("%06d", partNum) + "\n");
             }
             current.close();
         }
