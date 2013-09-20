37880ed543f1ce1a43a822526528841bee496afa
Julien Nioche <julien@digitalpebble.com>
2013-09-20 13:15:53 +0100

    Nutch converter takes dir as input + prints out timings

--- io/src/main/java/com/digitalpebble/behemoth/io/nutch/NutchSegmentConverterJob.java
+++ io/src/main/java/com/digitalpebble/behemoth/io/nutch/NutchSegmentConverterJob.java
@@ -18,11 +18,13 @@
 package com.digitalpebble.behemoth.io.nutch;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileInputFormat;
@@ -39,6 +41,9 @@ import org.apache.hadoop.util.ToolRunner;
 import org.apache.nutch.crawl.CrawlDatum;
 import org.apache.nutch.metadata.Nutch;
 import org.apache.nutch.protocol.Content;
+import org.apache.nutch.util.HadoopFSUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.digitalpebble.behemoth.BehemothConfiguration;
 import com.digitalpebble.behemoth.BehemothDocument;
@@ -49,97 +54,118 @@ import com.digitalpebble.behemoth.BehemothDocument;
  * parse or fetch metadata are stored in the document metadata.
  */
 public class NutchSegmentConverterJob extends Configured implements Tool,
-        Mapper<Text, Content, Text, BehemothDocument> {
+		Mapper<Text, Content, Text, BehemothDocument> {
 
-    public static final Logger LOG = LoggerFactory
-            .getLogger(NutchSegmentConverterJob.class);
+	public static final Logger LOG = LoggerFactory
+			.getLogger(NutchSegmentConverterJob.class);
 
-    public NutchSegmentConverterJob() {
-        this(null);
-    }
+	public NutchSegmentConverterJob() {
+		this(null);
+	}
 
-    public NutchSegmentConverterJob(Configuration conf) {
-        super(conf);
-    }
+	public NutchSegmentConverterJob(Configuration conf) {
+		super(conf);
+	}
 
-    public void configure(JobConf job) {
-        setConf(job);
-    }
+	public void configure(JobConf job) {
+		setConf(job);
+	}
 
-    public void close() {
-    }
+	public void close() {
+	}
 
-    private Text newKey = new Text();
+	public void map(Text key, Content content,
+			OutputCollector<Text, BehemothDocument> output, Reporter reporter)
+			throws IOException {
 
-    public void map(Text key, Content content,
-            OutputCollector<Text, BehemothDocument> output, Reporter reporter)
-            throws IOException {
+		BehemothDocument behemothDocument = new BehemothDocument();
 
-        BehemothDocument behemothDocument = new BehemothDocument();
+		int status = Integer.parseInt(content.getMetadata().get(
+				Nutch.FETCH_STATUS_KEY));
+		if (status != CrawlDatum.STATUS_FETCH_SUCCESS) {
+			// content not fetched successfully, skip document
+			LOG.debug("Skipping " + key
+					+ " as content is not fetched successfully");
+			return;
+		}
 
-        int status = Integer.parseInt(content.getMetadata().get(
-                Nutch.FETCH_STATUS_KEY));
-        if (status != CrawlDatum.STATUS_FETCH_SUCCESS) {
-            // content not fetched successfully, skip document
-            LOG.debug("Skipping " + key
-                    + " as content is not fetched successfully");
-            return;
-        }
+		// TODO store the fetch metadata in the Behemoth document
+		// store the binary content and mimetype in the Behemoth document
 
-        // TODO store the fetch metadata in the Behemoth document
-        // store the binary content and mimetype in the Behemoth document
+		String contentType = content.getContentType();
+		byte[] binarycontent = content.getContent();
+		behemothDocument.setUrl(key.toString());
+		behemothDocument.setContent(binarycontent);
+		behemothDocument.setContentType(contentType);
+		output.collect(key, behemothDocument);
+	}
 
-        String contentType = content.getContentType();
-        byte[] binarycontent = content.getContent();
-        behemothDocument.setUrl(key.toString());
-        behemothDocument.setContent(binarycontent);
-        behemothDocument.setContentType(contentType);
-        output.collect(key, behemothDocument);
-    }
+	public void convert(List<Path> list, Path output) throws IOException {
 
-    public void convert(Path nutchsegment, Path output) throws IOException {
+		JobConf job = new JobConf(getConf());
 
-        JobConf job = new JobConf(getConf());
-        job.setJobName("Convert Nutch segment" + nutchsegment);
-        job.setJarByClass(this.getClass());
+		job.setJobName("Converting Nutch segments");
+		job.setJarByClass(this.getClass());
 
-        FileInputFormat.addInputPath(job, new Path(nutchsegment,
-                Content.DIR_NAME));
-        job.set(Nutch.SEGMENT_NAME_KEY, nutchsegment.getName());
-        job.setInputFormat(SequenceFileInputFormat.class);
-        job.setMapperClass(NutchSegmentConverterJob.class);
+		for (Path p : list) {
+			FileInputFormat.addInputPath(job, new Path(p, Content.DIR_NAME));
+		}
 
-        // no reducers
-        job.setNumReduceTasks(0);
+		job.setInputFormat(SequenceFileInputFormat.class);
+		job.setMapperClass(NutchSegmentConverterJob.class);
 
-        FileOutputFormat.setOutputPath(job, output);
-        job.setOutputFormat(SequenceFileOutputFormat.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(BehemothDocument.class);
+		// no reducers
+		job.setNumReduceTasks(0);
 
-        JobClient.runJob(job);
-        if (LOG.isInfoEnabled()) {
-            LOG.info("Conversion: done");
-        }
-    }
+		FileOutputFormat.setOutputPath(job, output);
+		job.setOutputFormat(SequenceFileOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(BehemothDocument.class);
 
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(BehemothConfiguration.create(),
-                new NutchSegmentConverterJob(), args);
-        System.exit(res);
-    }
+		long start = System.currentTimeMillis();
+		JobClient.runJob(job);
+		long finish = System.currentTimeMillis();
+		if (LOG.isInfoEnabled()) {
+			LOG.info("NutchSegmentConverter completed. Timing: "
+					+ (finish - start) + " ms");
+		}
+	}
 
-    public int run(String[] args) throws Exception {
-        String usage = "Usage: SegmentConverter segment output";
+	public static void main(String[] args) throws Exception {
+		int res = ToolRunner.run(BehemothConfiguration.create(),
+				new NutchSegmentConverterJob(), args);
+		System.exit(res);
+	}
 
-        if (args.length == 0) {
-            System.err.println(usage);
-            System.exit(-1);
-        }
-        Path segment = new Path(args[0]);
-        Path output = new Path(args[1]);
-        convert(segment, output);
-        return 0;
-    }
+	public int run(String[] args) throws Exception {
+
+		String usage = "Usage: SegmentConverter [-dir segdir | segment] output";
+
+		if (args.length < 2) {
+			System.err.println(usage);
+			System.exit(-1);
+		}
+
+		final List<Path> segments = new ArrayList<Path>();
+
+		if (args[0].equals("-dir")) {
+			Path dir = new Path(args[1]);
+			FileSystem fs = dir.getFileSystem(getConf());
+			FileStatus[] fstats = fs.listStatus(dir,
+					HadoopFSUtil.getPassDirectoriesFilter(fs));
+			Path[] files = HadoopFSUtil.getPaths(fstats);
+			for (Path p : files) {
+				segments.add(p);
+			}
+		}
+
+		else {
+			segments.add(new Path(args[0]));
+		}
+
+		Path output = new Path(args[args.length - 1]);
+		convert(segments, output);
+		return 0;
+	}
 
 }
